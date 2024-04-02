2cc1f51ea41cff984214d403989deb0df2e4cf69
ramez <ramezhishamahmad@gmail.com>
2024-04-02 21:27:01 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // colors:{
+      //   yellow:'#f4d04e',
+      //   white:'#ffffff',
+      //   grey:'#808080',
+      //   black:'#121212',
+      // },
+      // fontFamily: {
+      //   Figtree: ['Figtree', 'sans-serif'],
+      // },
+      shadow:'11px 11px 7px 11px rgba(0,0,0,0.75);'
+    },
+  },
+  plugins: [],
+}
+
